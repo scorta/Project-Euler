9742f1d1da82888551ef408a58af5697d6f77192
scorta <scorta@users.noreply.github.com>
2017-04-25 14:24:30 +0700

    Solution for Project Euler problem 46

---
+++ ProjectEuler_46.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+/**
+ * Created by John on 24/04/2017.
+ */
+public class ProjectEuler_46 {
+    private static ArrayList<Long> primeList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Goldbach();
+    }
+
+    private static void Goldbach(){
+        long numberToCheck = 3L;
+        primeList.add(2L);
+
+        do {
+            if (!isPrime(numberToCheck)) {
+                if (!isGoldbach(numberToCheck)) {
+                    System.out.println(numberToCheck);
+                    break;
+                }
+            }
+            numberToCheck += 2;
+        }
+        while (true);
+    }
+
+    private static boolean isGoldbach(long number) {
+        for (long prime : primeList) {
+            for (long twiceSquare = 1; twiceSquare < Math.sqrt((number - prime) / 2) + 1; twiceSquare++) {
+                if (number == prime + 2 * twiceSquare * twiceSquare) return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isPrime(long number) {
+        for (Long prime : primeList) {
+            if (number % prime == 0L) return false;
+        }
+        primeList.add(number);
+        return true;
+    }
+}
